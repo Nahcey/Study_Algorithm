6c71217829d039c85d21577593987e905c82ccd6
Park YeChan <yechanpark05@gamil.com>
2025-07-08 12:43:59 +0900

    mod 3-2

--- 3/3-2.py
+++ 3/3-2.py
@@ -2,7 +2,7 @@
 
 from typing import Any, Sequence
 
-def seq_serach(a: Sequence, key: Any) -> int:
+def seq_search(a: Sequence, key: Any) -> int:
     """시퀀스 a에서 key와 같이 같은 원소를 선형 검색(for문)"""
     for i in range(len(a)):
         if a[i] == key:
